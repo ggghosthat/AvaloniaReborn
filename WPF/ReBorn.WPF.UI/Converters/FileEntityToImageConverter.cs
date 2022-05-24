fbd8eca3a4dac51dc51c22db95d6814d08c5bf9c
ggghosthat <ildarildar990@gmail.com>
2022-05-24 17:10:52 +0300

    Add tile representation of folders and files

---
+++ WPF/ReBorn.WPF.UI/Converters/FileEntityToImageConverter.cs
@@ -0,0 +1,37 @@
+﻿using Shared.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ReBorn.WPF.UI
+{
+    internal class FileEntityToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var drawingImage = new DrawingImage();
+
+            if(value is DirectoryViewModel) 
+            {
+                var directoryImageSource = Application.Current.TryFindResource("FolderIcon");
+                if (directoryImageSource is ImageSource directoryImageInstance)
+                    return directoryImageInstance;
+            }
+            else if (value is FileEntityViewModel)
+            {
+                var fileImageSource = Application.Current.TryFindResource("FileIcon");
+                if (fileImageSource is ImageSource fileImageInstance)
+                    return fileImageInstance;
+            }
+
+            return drawingImage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
