a528369aff710f055478a741e00388bbc8902364
ggghosthat <ildarildar990@gmail.com>
2022-06-15 19:57:08 +0300

    Update title bar

---
+++ WPF/ReBorn.WPF.UI/Converters/WindowTitleHeightConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ReBorn.WPF.UI.Converters
+{
+    internal class WindowTitleHeightConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is WindowState windowState)
+                return windowState == WindowState.Normal ? 42 : 32;
+
+            return 42;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
